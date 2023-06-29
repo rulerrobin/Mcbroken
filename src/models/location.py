34a89601a8de3c13415dd057cbb27c32c1cdb8ec
Genuel Mark <gmzapanta98@gmail.com>
2023-06-29 16:53:10 +1000

    added model and schema with relationships to report table

---
+++ src/models/location.py
@@ -0,0 +1,25 @@
+# location schema
+from init import db, ma
+from marshmallow import fields
+
+class Location(db.Model):
+    __tablename__ = 'locations'
+
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.String(10), nullable = False)
+    street = db.Column(db.String(100), nullable = False)
+    postcode = db.Column(db.String(10), nullable = False)
+    suburb = db.Column(db.String(100), nullable = False)
+    state = db.Column(db.String(20), nullable = False)
+
+
+    # Do cascade deletes later for links
+    reports = db.relationship('Report', back_populates='location', cascade='all, delete') # Relates to report model in db
+
+# Returning userSchema is only for admins unless searched username is same as user
+class LocationSchema(ma.Schema):
+    # Does not need to be linked to anything as it is linked as a DB for any future locations for reports
+
+    class Meta:
+        fields = ('id','number', 'street', 'postcode', 
+        'suburb', 'state')
