49eca26533d90d29ecaff234ab82fbafa551d951
Genuel Mark <gmzapanta98@gmail.com>
2023-06-29 16:33:10 +1000

    added user.py

--- src/models/user.py
+++ src/models/user.py
@@ -1,1 +1,21 @@
-# user schema
+# user model and schema 
+from init import db, ma
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), nullable=False, unique=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+    # Do cascade deletes later for links
+
+# Returning userSchema is only for admins unless searched username is same as user
+class UserSchema(ma.Schema):
+
+
+    class Meta:
+        fields = ('username', 'email', 'password')
