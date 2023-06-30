1b9192c275ed827fd73552aff9bb8e9e1ba1f14c
Genuel Mark <gmzapanta98@gmail.com>
2023-06-30 10:53:22 +1000

    added user relationship

--- src/models/report.py
+++ src/models/report.py
@@ -11,10 +11,12 @@ class Report(db.Model):
 
     # Foreign Keys
     location_id = db.Column(db.Integer, db.ForeignKey('locations.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
     # Do cascade deletes later for links
     # Relationships
-    location = db.relationship('Location', back_populates='reports')
-
+    location = db.relationship('Location', back_populates='reports', cascade='all, delete')
+    user = db.relationship('User', back_populates='reports', cascade='all, delete')
 
 # Returning userSchema is only for admins unless searched username is same as user
 class ReportSchema(ma.Schema):
@@ -22,9 +24,9 @@ class ReportSchema(ma.Schema):
     # Schema Connections
 
     # Location first as to show where 
-    location = fields.List(fields.Nested('locationSchema'))
-
+    location = fields.List(fields.Nested('LocationSchema'))
     comments = fields.List(fields.Nested('CommentSchema', exclude=['id']))
+    user = fields.Nested('UserSchema', only=('username'))
 
     class Meta:
-        fields = ('id','time_reported', 'comments')
+        fields = ('id','user','time_reported', 'comments')
