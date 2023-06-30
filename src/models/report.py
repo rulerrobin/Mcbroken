2059ed2b5f6a50dd03b9276296e6d6c7b97ec7e4
Genuel Mark <gmzapanta98@gmail.com>
2023-06-30 11:21:50 +1000

    finished adding relationships to comment

--- src/models/report.py
+++ src/models/report.py
@@ -7,26 +7,26 @@ class Report(db.Model):
     __tablename__ = 'reports'
 
     id = db.Column(db.Integer, primary_key=True)
-    time_reported = db.Column(db.DateTime, default=datetime.utcnow)
+    time_reported = db.Column(db.DateTime, default=datetime.utcnow) # Time and date posted
 
     # Foreign Keys
     location_id = db.Column(db.Integer, db.ForeignKey('locations.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    comment_id = db.Column(db.Integer, db.ForeignKey('comments.id'))
 
     # Do cascade deletes later for links
     # Relationships
     location = db.relationship('Location', back_populates='reports', cascade='all, delete')
     user = db.relationship('User', back_populates='reports', cascade='all, delete')
+    comments = db.relationship('Comment', back_populates='reports', cascade='all, delete')
 
 # Returning userSchema is only for admins unless searched username is same as user
 class ReportSchema(ma.Schema):
 
     # Schema Connections
-
-    # Location first as to show where 
     location = fields.List(fields.Nested('LocationSchema'))
-    comments = fields.List(fields.Nested('CommentSchema', exclude=['id']))
     user = fields.Nested('UserSchema', only=('username'))
+    comments = fields.List(fields.Nested('CommentSchema', exclude=['id']))
 
     class Meta:
-        fields = ('id','user','time_reported', 'comments')
+        fields = ('id', 'location','user','time_reported', 'comments')
